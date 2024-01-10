9f4b34d5bfe78f18ec835283b02a616b2f69c448
adnier87 <adnier87@gmail.com>
2024-01-10 13:40:09 -0600

    Create input component for searching

---
+++ src/components/input-searcher.component.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+
+const InputSearcher: React.FC<{ handleTerm: (term: string) => void }> = ({ handleTerm }) => {
+  const [term, setTerm] = useState('');
+
+  const handleSearch = () => {
+    handleTerm(term);
+  };
+
+  const handleKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div className='relative'>
+      <input
+        className="bg-[#F4F4F4] rounded-md pr-10 pl-3 w-full h-12 border border-transparent focus-visible:border-purple-500 transition-colors duration-200 ease-in-out outline-0"
+        type="text"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
+      <button className='absolute right-3 h-full' onClick={handleSearch} title="Search">
+        <MagnifyingGlassIcon className="w-5 text-purple-500" />
+      </button>
+    </div>
+  );
+};
+
+export default InputSearcher;
