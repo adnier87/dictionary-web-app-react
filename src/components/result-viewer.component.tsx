1818cf7b43ab6c2c2178fbca154463a1469fb4e6
adnier87 <adnier87@gmail.com>
2024-01-18 12:42:05 -0600

    Apply dark-mode style to results

--- src/components/result-viewer.component.tsx
+++ src/components/result-viewer.component.tsx
@@ -18,23 +18,23 @@ const ResultViewer: React.FC<ResultViewerProps> = ({ data : { word, meanings, ph
     <section>
       <header className='my-11 flex'>
         <div>
-          <h1 className="text-6xl font-bold">{ word }</h1>
-          <p className='text-2xl'>{phonetics[0].text}</p>
+          <h1 className="text-6xl font-bold text-neutral-800 dark:text-white">{ word }</h1>
+          <p className='text-2xl text-secondary'>{phonetics[0].text}</p>
         </div>
       </header>
       <div>
         {meanings.map((meaning, index) => (
           <div key={index}>
             <header className='flex flex-wrap items-center my-5'>
-              <h3 className="text-2xl font-bold leading-none italic mr-5">{ meaning.partOfSpeech }</h3>
+              <h3 className="text-2xl font-bold leading-none italic mr-5 text-neutral-800 dark:text-white">{ meaning.partOfSpeech }</h3>
               <hr className='grow' />
             </header>
             <section className='py-5'>
-              <h4 className='text-xl mb-5'>Meaning</h4>
+              <h4 className='text-xl mb-5 text-neutral-500'>Meaning</h4>
               <ul className='list-disc pl-10'>
                 {meaning.definitions.map((definition, index) => (
-                  <li className='text-lg my-2' key={index}>
-                    { definition.definition }
+                  <li className='text-lg my-2 text-secondary' key={index}>
+                    <span className='text-neutral-800 dark:text-white'>{ definition.definition }</span>
                     { !_.isEmpty(definition.example) && <div className='text-lg text-neutral-500'>"{ definition.example }"</div> }
                   </li>
                 ))}
@@ -45,7 +45,7 @@ const ResultViewer: React.FC<ResultViewerProps> = ({ data : { word, meanings, ph
                 <h4 className='text-xl mb-5 mr-5 text-neutral-500'>Synonyms</h4>
                 <div>
                   {meaning.synonyms.map((synonym, index) => (
-                    <span key={index} className='text-lg mr-2 font-bold hover:cursor-pointer hover:underline'>{ synonym }</span>
+                    <span key={index} className='text-lg mr-2 font-bold hover:cursor-pointer hover:underline dark:text-white'>{ synonym }</span>
                   ))}
                 </div>
               </section>
@@ -55,7 +55,7 @@ const ResultViewer: React.FC<ResultViewerProps> = ({ data : { word, meanings, ph
                 <h4 className='text-xl mb-5 mr-5 text-neutral-500'>Antonyms</h4>
                 <div>
                   {meaning.antonyms.map((antonym, index) => (
-                    <span key={index} className='text-lg mr-2 font-bold hover:cursor-pointer hover:underline'>{ antonym }</span>
+                    <span key={index} className='text-lg mr-2 font-bold hover:cursor-pointer hover:underline dark:text-white'>{ antonym }</span>
                   ))}
                 </div>
               </section>
@@ -71,7 +71,7 @@ const ResultViewer: React.FC<ResultViewerProps> = ({ data : { word, meanings, ph
               {Array.from(sources).map((url, index) => (
                 <li key={index} className='text-neutral-800'>
                   <a href={url} target="_blank" rel="noopener noreferrer" className='flex items-center group'>
-                    <span className='mr-1 group-hover:underline'>{ url }</span>
+                    <span className='mr-1 group-hover:underline text-nuetral-800 dark:text-white'>{ url }</span>
                     <ArrowTopRightOnSquareIcon className="h-3.5 text-neutral-500" />
                   </a>
                 </li>
