7a48c3b576d6a6142ac7ba15729878559806cb82
adnier87 <adnier87@gmail.com>
2024-01-18 12:25:15 -0600

    Apply dark-mode style to input

--- src/components/input-searcher.component.tsx
+++ src/components/input-searcher.component.tsx
@@ -17,14 +17,14 @@ const InputSearcher: React.FC<{ handleTerm: (term: string) => void }> = ({ handl
   return (
     <div className='relative'>
       <input
-        className="bg-[#F4F4F4] rounded-md pr-10 pl-3 w-full h-12 border border-transparent focus-visible:border-purple-500 transition-colors duration-200 ease-in-out outline-0"
+        className="bg-[#F4F4F4] dark:bg-[#1F1F1F] text-neutral-800 dark:text-white rounded-md pr-10 pl-3 w-full h-12 border border-transparent focus-visible:border-purple-500 transition-colors duration-200 ease-in-out outline-0"
         type="text"
         value={term}
         onChange={(e) => setTerm(e.target.value)}
         onKeyUp={handleKeyUp}
       />
       <button className='absolute right-3 h-full' onClick={handleSearch} title="Search">
-        <MagnifyingGlassIcon className="w-5 text-purple-500 dark:text-gray-300" />
+        <MagnifyingGlassIcon className="w-5 text-secondary" />
       </button>
     </div>
   );
