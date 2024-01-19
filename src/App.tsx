f5219a4c9608e9a8eb37f25fcd1820ca2abef0a6
adnier87 <adnier87@gmail.com>
2024-01-19 14:30:50 -0600

    Add functionality of changing fonts

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,43 @@ import React from 'react';
 
 import book from './assets/book.svg';
 import './App.scss';
-import { FontProvider } from './contexts/font.context';
+import { FontContext, FontProvider } from './contexts/font.context';
 import FontSelector from './components/fonts-selector.component';
 import ThemeSwitcher from './components/theme-switcher.component';
 import { ThemeContext, ThemeProvider } from './contexts/theme.context';
 import SearcherView from './views/searcher.view';
-import { Theme } from './constants';
+import { FontType, Theme } from './constants';
+
+const FontMap = {
+  Serif: 'font-serif',
+  SansSerif: 'font-sans',
+  Monospace: 'font-mono',
+}
 
 function App() {
-
   return (
     <ThemeProvider>
       <FontProvider>
         <ThemeContext.Consumer>
           {({ theme }) => (
-            <div className={`${theme === Theme.Dark ? 'dark' : ''} min-h-screen`}>
-              <div className='container mx-auto pt-14'>
-                <header className='w-full max-w-[737px] mx-auto flex justify-between'>
-                  <img src={book} alt='book' className='w-8 h-8' />
-                  <div className='flex items-center divide-x'>
-                    <div className='mr-5'><FontSelector /></div>
-                    <div className='pl-5'><ThemeSwitcher /></div>
-                  </div>
-                </header>
-                <main className='w-full max-w-[737px] mx-auto pt-12'>
-                  <SearcherView />
-                </main>
+            <FontContext.Consumer>
+              {({ font }) => (
+                <div className={`${theme === Theme.Dark ? 'dark' : ''} min-h-screen ${FontMap[font as keyof typeof FontType]}`}>
+                <div className='container mx-auto pt-14'>
+                  <header className='w-full max-w-[737px] mx-auto flex justify-between'>
+                    <img src={book} alt='book' className='w-8 h-8' />
+                    <div className='flex items-center divide-x'>
+                      <div className='mr-5'><FontSelector /></div>
+                      <div className='pl-5'><ThemeSwitcher /></div>
+                    </div>
+                  </header>
+                  <main className='w-full max-w-[737px] mx-auto pt-12'>
+                    <SearcherView />
+                  </main>
+                </div>
               </div>
-            </div>
+              )}
+            </FontContext.Consumer>
           )}
         </ThemeContext.Consumer>
       </FontProvider>
